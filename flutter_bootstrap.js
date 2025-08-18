87fa310f3dfebe331cdb2213483c0d69adcb3f3f
kenkaroki <kenkaroki@users.noreply.github.com>
2025-08-18 07:30:52 +0000

    deploy: 7c328b0601d1df0502428969fa9120e463380fb7

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "39921492"
+    serviceWorkerVersion: "3135704545"
   }
 });
